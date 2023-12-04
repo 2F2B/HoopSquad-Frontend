505e0d9948dd3f027fc8555aafb185c0c0c7dba4
OhSeChan <seb_s19@naver.com>
2023-12-05 03:25:58 +0900

    :sparkles: LogBox 추가
    - "Possible Unhandled Promise Rejection" Log가 사용자 화면에 나오지
      않도록 처리

--- hoopsquad_FE/src/App.js
+++ hoopsquad_FE/src/App.js
@@ -1,5 +1,8 @@
 import { UserProvider } from "./contexts/UserContext";
 import Navigation from "./navigations/Navigation";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["Possible Unhandled Promise Rejection"]);
 
 export default function App() {
   return (
