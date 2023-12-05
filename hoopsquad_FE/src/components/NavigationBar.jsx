151443143bad203e3b0a1ac308cb6f0c6aeefa96
0Jaemin0 <jaemin2167@naver.com>
2023-12-06 00:44:38 +0900

    :sparkles: 네비게이션바 프로필 이동 추가
    - StyleSheet 사용

--- hoopsquad_FE/src/components/NavigationBar.jsx
+++ hoopsquad_FE/src/components/NavigationBar.jsx
@@ -8,72 +8,59 @@ import Profile from "../../assets/Profile.png";
 
 const NavigationBar = (props) => {
   const navigation = useNavigation();
+
   return (
     <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        position: "absolute",
-        bottom: 0,
-        borderTopWidth: 1,
-        height: 65,
-        borderTopColor: props.opacity ? "rgba(0, 0, 0, 0.1)" : "#E2E2E2",
-        backgroundColor: props.opacity ? "#878787" : "#ffffff",
-      }}
+      style={[
+        styles.navigationBarContainer,
+        { borderTopColor: props.opacity ? "rgba(0, 0, 0, 0.1)" : "#E2E2E2" },
+        { backgroundColor: props.opacity ? "#878787" : "#ffffff" },
+      ]}
     >
-      <TouchableOpacity
-        style={{
-          height: 35,
-          width: "20%",
-          alignItems: "center",
-        }}
-      >
+      <TouchableOpacity style={styles.navigationBox}>
         <Image
           source={Team}
           resizeMode="contain"
-          style={{ width: "100%", height: "100%", marginTop: 5 }}
+          style={styles.navigationImg}
         ></Image>
         <Text>팀</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={{ height: 35, width: "20%", alignItems: "center" }}
-      >
+      <TouchableOpacity style={styles.navigationBox}>
         <Image
           source={Chat}
           resizeMode="contain"
-          style={{ width: "100%", height: "100%", marginTop: 5 }}
+          style={styles.navigationImg}
         ></Image>
         <Text>채팅</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={{ height: 35, width: "20%", alignItems: "center" }}
+        style={styles.navigationBox}
         onPress={() => navigation.navigate("Match")}
       >
         <Image
           source={Matching}
           resizeMode="contain"
-          style={{ width: "100%", height: "100%", marginTop: 5 }}
+          style={styles.navigationImg}
         ></Image>
         <Text>매칭</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={{ height: 35, width: "20%", alignItems: "center" }}
-      >
+      <TouchableOpacity style={styles.navigationBox}>
         <Image
           source={BasketBallCourt}
           resizeMode="contain"
-          style={{ width: "100%", height: "100%", marginTop: 5 }}
+          style={styles.navigationImg}
         ></Image>
         <Text>농구장</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={{ height: 35, width: "20%", alignItems: "center" }}
+        style={styles.navigationBox}
+        onPress={() => navigation.navigate("Profile")}
       >
         <Image
           source={Profile}
           resizeMode="contain"
-          style={{ width: "100%", height: "100%", marginTop: 5 }}
+          style={styles.navigationImg}
         ></Image>
         <Text>프로필</Text>
       </TouchableOpacity>
@@ -85,4 +72,24 @@ NavigationBar.defaultProps = {
   opacity: false,
 };
 
+const styles = StyleSheet.create({
+  navigationBarContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    bottom: 0,
+    borderTopWidth: 1,
+    height: 65,
+  },
+  navigationBox: {
+    height: 35,
+    width: "20%",
+    alignItems: "center",
+  },
+  navigationImg: {
+    width: "100%",
+    height: "100%",
+    marginTop: 5,
+  },
+});
 export default NavigationBar;
