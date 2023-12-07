2e25ea6c00b712bfd1a0764976104bb4dce14d03
OhSeChan <seb_s19@naver.com>
2023-12-07 09:15:05 +0900

    :sparkles: ChatRoomItem 컴포넌트 추가
    - 채팅방 목록 중 하나의 Item인 ChatRoomItem 컴포넌트 구현
    - ChatList에서 채팅방에 대한 정보와 소켓 props로 받아 채팅방 정보를 구성
    - SocketContext에서 채팅 리스트 갱신을 위한 setChatList
    - 터치 시 postingId를 전달하고 postingId 채팅방의 채팅 리스트를 콜백으로
      전달 받아 setChatList로 최신화 및 채팅방으로 navigate
    - naviagte 시 postingId, postingTitle, image를 전달

---
+++ hoopsquad_FE/src/screens/Chat/components/ChatRoomItem.jsx
@@ -0,0 +1,111 @@
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { REACT_APP_PROXY } from "@env";
+import { useNavigation } from "@react-navigation/native";
+import SocketContext from "../../../contexts/SocketContext";
+import formatDate from "../../../utils/formatDate";
+import { useContext } from "react";
+
+const ChatRoomItem = ({ item, socketRef }) => {
+  const {
+    image,
+    lastChatMessage,
+    lastChatTime,
+    nickName,
+    postingId,
+    postingTitle,
+  } = item;
+
+  const navigation = useNavigation();
+  const { setChatList } = useContext(SocketContext);
+
+  const enterRoom = () => {
+    socketRef.current.emit("enterRoom", postingId, (chatList) => {
+      setChatList((prevChatList) => ({
+        ...prevChatList,
+        [postingId]: chatList,
+      }));
+      navigation.navigate("ChatRoom", {
+        postingId,
+        postingTitle,
+        image,
+      });
+    });
+  };
+
+  return (
+    <TouchableOpacity
+      style={styles.chat}
+      onPress={() => {
+        enterRoom();
+      }}
+    >
+      <View style={styles.imgWrapper}>
+        <Image
+          resizeMode="cover"
+          source={{
+            uri: `${REACT_APP_PROXY}image/match/${image}`,
+          }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </View>
+      <View style={styles.chatContent}>
+        <View style={styles.chatLayout}>
+          <Text style={styles.userNickname}>{postingTitle}</Text>
+          <Text style={styles.timeFont}>{formatDate(lastChatTime, true)}</Text>
+        </View>
+        <View style={styles.chatLayout}>
+          <Text>{lastChatMessage}</Text>
+          <Text style={styles.chatCount}>{0}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  chatLayout: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  chat: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderColor: "#E2E2E2",
+  },
+  imgWrapper: {
+    width: 80,
+    aspectRatio: 1 / 1,
+    borderRadius: 40,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#E2E2E2",
+  },
+  chatContent: {
+    flex: 1,
+    justifyContent: "space-around",
+    marginLeft: 16,
+  },
+  userNickname: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  timeFont: {
+    fontSize: 10,
+  },
+  chatCount: {
+    color: "#ffffff",
+    backgroundColor: "#F3A241",
+    width: 20,
+    fontSize: 12,
+    aspectRatio: 1 / 1,
+    borderRadius: 40,
+    textAlign: "center",
+  },
+});
+
+export default ChatRoomItem;
