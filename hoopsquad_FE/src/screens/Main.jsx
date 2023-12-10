0da89c57a26af2d0e3cbdc0a8fd4cb0000ed025b
OhSeChan <seb_s19@naver.com>
2023-12-10 13:40:53 +0900

    :sparkles: pushToken 등록 및 알림 이동 버튼 추가
    - pushToken을 userId 기반으로 저장하기 위한 sendPushToken 함수 추가
    - 알림 페이지로 이동시키기 위한 버튼 추가

--- hoopsquad_FE/src/screens/Main.jsx
+++ hoopsquad_FE/src/screens/Main.jsx
@@ -1,10 +1,36 @@
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import NavigationBar from "../components/NavigationBar";
 import Usercontext from "../contexts/UserContext";
+import { usePushNotifications } from "../hooks/usePushNotification";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { REACT_APP_PROXY } from "@env";
 
 const Main = () => {
   const { user, logout } = useContext(Usercontext);
+  const { expoPushToken } = usePushNotifications();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (user.User_id && expoPushToken) {
+      sendPushToken();
+    }
+  }, []);
+
+  const sendPushToken = async () => {
+    try {
+      const res = await axios.post(
+        `${REACT_APP_PROXY}notification/registerPushToken`,
+        {
+          userId: user.User_id,
+          token: expoPushToken,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -16,8 +42,14 @@ const Main = () => {
         <TouchableOpacity style={styles.buttonStyle} onPress={() => logout()}>
           <Text style={styles.buttonText}>로그아웃</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => navigation.navigate("Notification")}
+        >
+          <Text style={styles.buttonText}>알림</Text>
+        </TouchableOpacity>
       </View>
-      <NavigationBar/>
+      <NavigationBar />
     </View>
   );
 };
@@ -26,10 +58,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-    margin : 20,
+    margin: 20,
   },
   center: {
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   buttonStyle: {
     height: 40,
