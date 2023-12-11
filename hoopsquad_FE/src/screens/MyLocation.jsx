e7af8955447420dd83fc3f847573d062e0e973d4
0Jaemin0 <jaemin2167@naver.com>
2023-12-11 22:30:18 +0900

    :sparkles: 지역 선택 페이지 구현
    - 원하는 지역을 선택 할 수 있는 페이지 구현
    - 최소 1개 최대 2개의 지역 선택 가능

---
+++ hoopsquad_FE/src/screens/MyLocation.jsx
@@ -0,0 +1,253 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
+import locationConfig from "../components/locationConfig";
+import { useState, useContext } from "react";
+import Usercontext from "../contexts/UserContext";
+import authApi from "../apis/authApi";
+import { REACT_APP_PROXY } from "@env";
+
+const MyLocation = () => {
+  const navigation = useNavigation();
+  const { user, setUser } = useContext(Usercontext);
+  const [startLocation, setStartLocation] = useState("강원도");
+  const [firstLocation, setFirstLocation] = useState({
+    location: user.Location1.location,
+    City: user.Location1.City,
+  });
+  const [secondLocation, setSecondLocation] = useState({
+    location: user.Location2.location,
+    City: user.Location2.City,
+  });
+
+  const regions = Object.keys(locationConfig);
+
+  const selectLocation = (selectLocation, selectCity) => {
+    if (
+      (firstLocation.City === selectCity &&
+        firstLocation.location === selectLocation) ||
+      (secondLocation.City === selectCity &&
+        secondLocation.location === selectLocation)
+    ) {
+      if (
+        firstLocation.City === selectCity &&
+        firstLocation.location === selectLocation
+      ) {
+        setFirstLocation({ location: null, City: null });
+        if (secondLocation !== null) {
+          setFirstLocation(secondLocation);
+          setSecondLocation({ location: null, City: null });
+        }
+      } else {
+        setSecondLocation({ location: null, City: null });
+      }
+    } else {
+      if (firstLocation.City === null) {
+        setFirstLocation({ location: selectLocation, City: selectCity });
+      } else {
+        if (secondLocation.City === null) {
+          setSecondLocation({ location: selectLocation, City: selectCity });
+        }
+      }
+    }
+  };
+
+  const locationRegister = async () => {
+    try {
+      await authApi.post(`${REACT_APP_PROXY}location`, {
+        Location1: {
+          location: firstLocation.location,
+          city: firstLocation.City,
+        },
+        Location2: {
+          location: secondLocation.location,
+          city: secondLocation.City,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={[{ flex: 1 }]}>
+      <StatusBar style="dark" />
+      <View style={styles.header}>
+        <View style={styles.headerTextWrapper}>
+          <TouchableOpacity>
+            <Ionicons
+              name="chevron-back"
+              size={30}
+              color="black"
+              style={{ marginTop: 5 }}
+            />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>우리 지역 설정</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              if (firstLocation.City !== null) {
+                setUser((prevUser) => ({
+                  ...prevUser,
+                  Location1: {
+                    location: firstLocation.location,
+                    City: firstLocation.City,
+                  },
+                  Location2: {
+                    location: secondLocation.location,
+                    City: secondLocation.City,
+                  },
+                }));
+                locationRegister();
+                navigation.navigate("Main");
+              }
+            }}
+          >
+            <Text style={styles.headerTextSuccess}>완료</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.matchingContainer}>
+        <Text style={styles.matchingTitle}>우리 지역</Text>
+        <Text style={styles.matchingSubTitle}>
+          활동을 주로 진행할 지역을 선택해주세요
+        </Text>
+        <Text>
+          현재 선택 지역 {firstLocation?.City} {secondLocation?.City}
+        </Text>
+      </View>
+      <View style={{ flexDirection: "row", flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.locationWrapper}>
+            <View style={{ flex: 1 }}>
+              {regions.map((region, index) => (
+                <TouchableOpacity
+                  onPress={() => setStartLocation(region)}
+                  key={index}
+                  style={[
+                    styles.locationName,
+                    {
+                      backgroundColor:
+                        regions[index] === startLocation
+                          ? "#F3A241"
+                          : "#E2E2E2",
+                    },
+                  ]}
+                >
+                  <Text style={{ color: "white" }}>{region}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+        <ScrollView style={{ flex: 1 }}>
+          <View>
+            {locationConfig[startLocation].map((city, index) => (
+              <TouchableOpacity
+                style={styles.cityName}
+                key={index}
+                onPress={() => selectLocation(startLocation, city)}
+              >
+                <View style={styles.headerTextWrapper}>
+                  <AntDesign
+                    name="checkcircle"
+                    size={20}
+                    color={
+                      (firstLocation.City === city &&
+                        firstLocation.location === startLocation) ||
+                      (secondLocation.City === city &&
+                        secondLocation.location === startLocation)
+                        ? "#F3A241"
+                        : "#CDCDCD"
+                    }
+                    style={{ marginRight: 10 }}
+                  />
+                  <Text
+                    style={{
+                      color:
+                        (firstLocation.City === city &&
+                          firstLocation.location === startLocation) ||
+                        (secondLocation.City === city &&
+                          secondLocation.location === startLocation)
+                          ? "black"
+                          : "#CDCDCD",
+                    }}
+                  >
+                    {city}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    paddingVertical: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 0.5,
+    paddingBottom: 15,
+    justifyContent: "space-between",
+    borderBottomColor: "#E2E2E2",
+  },
+  headerTextWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  headerTextSuccess: {
+    color: "#F3A241",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  matchingContainer: {
+    padding: 20,
+  },
+  matchingTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 5,
+  },
+  matchingSubTitle: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#8F8F8F",
+    marginBottom: 15,
+  },
+  locationWrapper: {
+    flexDirection: "row",
+    flex: 1,
+  },
+  locationName: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderWidth: 0.5,
+    borderColor: "white",
+  },
+  cityName: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+});
+export default MyLocation;
