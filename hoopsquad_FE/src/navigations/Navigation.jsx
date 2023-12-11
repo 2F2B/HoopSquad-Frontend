e42747dca78d65beb2b99f7169c23cae30475348
0Jaemin0 <jaemin2167@naver.com>
2023-12-11 22:27:19 +0900

    :sparkles: 지역 선택 네비게이션 추가

--- hoopsquad_FE/src/navigations/Navigation.jsx
+++ hoopsquad_FE/src/navigations/Navigation.jsx
@@ -19,7 +19,7 @@ import Notification from "../screens/Notification/Notification";
 import GoogleMapPin from "../components/GoogleMapPin";
 import ProfileRegister from "../screens/ProfileRegister/ProfileRegister";
 import MyProfile from "../screens/Profile/MyProfile";
-
+import MyLocation from "../screens/MyLocation";
 const Navigation = () => {
   const { user, setUser } = useContext(Usercontext);
   const Stack = createNativeStackNavigator();
@@ -53,15 +53,19 @@ const Navigation = () => {
       <Stack.Navigator
         screenOptions={{ contentStyle: { backgroundColor: "white" } }}
       >
-        {user ? (
+        {!user && <Stack.Screen name="Login" component={Login} />}
+        {user && user.Location1.City === null && (
           <Stack.Screen
-            name="Main"
-            component={Main}
+            name="MyLocation"
+            component={MyLocation}
             options={{ headerShown: false }}
           />
-        ) : (
-          <Stack.Screen name="Login" component={Login} />
         )}
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Match"
           component={Matching}
